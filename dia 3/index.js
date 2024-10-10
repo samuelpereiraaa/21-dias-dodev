0c4943be88ecaed8a398f87533e6c5ea6754a3c7
Samuel Pereira <samuelpereira.dev@gmail.com>
2024-10-10 01:46:56 -0300

    Aprendendo variáveis, e fazendo um sistema.

---
+++ dia 3/index.js
@@ -0,0 +1,15 @@
+// Solicitando informações para guardar nas variáveis.
+
+let usuarioNome = prompt("digite seu nome");
+let usuarioIdade = parseInt (prompt("digite sua idade"));
+let usuarioAltura = parseFloat (prompt("digite sua altura"));
+let usuarioPeso = parseFloat (prompt("digite seu peso"));
+
+// Calculo para o nascimento, e o IMC.
+
+let nascimento = 2023 - usuarioIdade;
+let imc = usuarioPeso / (usuarioAltura * usuarioAltura);
+
+// Imprimir no console as informações obtidas com as entradas de dados e os calculos.
+
+console.log("Olá " + usuarioNome + ", você tem " + usuarioIdade + "anos, nasceu em " + nascimento + ",tem " + usuarioAltura + ",pesa " + usuarioPeso + "kg seu IMC é\n " + imc + "Kg/m2." );
