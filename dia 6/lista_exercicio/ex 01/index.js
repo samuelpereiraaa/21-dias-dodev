09f261d3bfe05ac912029a61d42b2530c644c059
Samuel Pereira <samuelpereira.dev@gmail.com>
2024-10-15 18:05:20 -0300

    loop for

---
+++ dia 6/lista_exercicio/ex 01/index.js
@@ -0,0 +1,6 @@
+let valor = parseInt(prompt("digite um valor: "))
+
+for(let i = 0;i<=valor;i++){
+    console.log(i+' ... '+ valor)
+}
+
---
+++ dia 6/lista_exercicio/ex 04/index.js
@@ -0,0 +1,11 @@
+let numero = Number(prompt('digite um numero: '))
+
+for(let i=numero; i<=numero+2 ;i++){
+    console.log('tabuada do numero: ' + i)
+for(j=0;j<=10;j++){
+    console.log(i + '*' + j + '=', (i*j))
+}    
+}
+
+
+ 
